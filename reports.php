98cf83735f86a3ab4ca746c75d9dfd7d47be8789
Aedix <66688859+Aedix@users.noreply.github.com>
2026-05-18 04:36:38 +0200

    Load rich report editor assets

--- reports.php
+++ reports.php
@@ -34,6 +34,7 @@ $activeServiceLogo = (string) ($user['active_service_logo'] ?? '');
   <link rel="stylesheet" href="/style.css?v=11" />
   <link rel="stylesheet" href="/mdt.css?v=7" />
   <link rel="stylesheet" href="/reports.css?v=3" />
+  <link rel="stylesheet" href="/reports-rich-editor.css?v=1" />
   <link rel="stylesheet" href="/reports-arrestation-dossier.css?v=1" />
   <link rel="stylesheet" href="/mdt-status-highlights.css?v=1" />
 </head>
@@ -160,6 +161,7 @@ $activeServiceLogo = (string) ($user['active_service_logo'] ?? '');
     });
   </script>
   <script src="/reports.js?v=6"></script>
+  <script src="/reports-rich-editor.js?v=1"></script>
   <script src="/reports-runtime-fixes.js?v=2"></script>
   <script src="/reports-arrestation-dossier.js?v=1"></script>
   <script src="/reports-status-editor.js?v=1"></script>
